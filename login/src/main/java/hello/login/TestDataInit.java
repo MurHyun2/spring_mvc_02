e076ce431b5ec3c4f10b14aae8f535002f6cec02
MurHyun2 <eddy152264@gmail.com>
2025-05-05 00:02:32 +0900

    섹션 7. 로그인 기능

--- login/src/main/java/hello/login/TestDataInit.java
+++ login/src/main/java/hello/login/TestDataInit.java
@@ -26,7 +26,7 @@ public class TestDataInit {
 
         Member member = new Member();
         member.setLoginId("test");
-        member.setLoginId("12");
+        member.setPassword("12");
         member.setName("테스터");
         memberRepository.save(member);
     }
